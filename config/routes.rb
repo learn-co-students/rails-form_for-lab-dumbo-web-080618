9b5b69544c29c01c6f6774b37eda4aec1d5436dd
vavnilov <vadim.avnilov@gmail.com>
2018-09-04 11:18:09 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :students, only: [:index,:new,:create,:edit,:update, :show]
+  resources :school_classes, only: [:index,:new,:create,:edit,:update, :show]
 end
